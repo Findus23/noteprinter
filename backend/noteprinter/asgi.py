2b2ad86d4ae7668d5d92bc404deb4894fbe7cc43
Lukas Winkler <git@lw1.at>
2025-07-14 23:06:13 +0200

    fix asgi.py

--- backend/noteprinter/asgi.py
+++ backend/noteprinter/asgi.py
@@ -9,17 +9,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noteprinter.settings")
+
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 from notes.consumers import PrintConsumer, RenderConsumer
 from notes.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noteprinter.settings")
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
